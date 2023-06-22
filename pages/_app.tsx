1d75c6aa62b0cc36a49d0c781d74ca18088d8f2a
Shigaichi <102985195+Shigaichi@users.noreply.github.com>
2023-06-22 22:58:19 +0900

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import MainLayout from '../components/layout/mainLayout'
 import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
