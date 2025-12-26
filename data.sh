314036c01ae6b7b34e5eb9af7550a6c716d8ed3f
shigaichi <102985195+shigaichi@users.noreply.github.com>
2025-12-27 00:03:30 +0900

    Add GitHub Actions workflow for Docker build and publish to GHCR

--- data.sh
+++ data.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -eu
 
-rm -rf data
-mv data2 data
+: "${AWS_ACCESS_KEY_ID:?AWS_ACCESS_KEY_ID is required}"
+: "${AWS_SECRET_ACCESS_KEY:?AWS_SECRET_ACCESS_KEY is required}"
+: "${S3_ENDPOINT:?S3_ENDPOINT is required}"
+: "${S3_BUCKET:?S3_BUCKET is required}"
+: "${S3_FILE:?S3_FILE is required}"
+
+aws --quiet --endpoint-url "${S3_ENDPOINT}" s3 cp "s3://${S3_BUCKET}/${S3_FILE}" ./
+
+rm -rf data/
+tar xf "${S3_FILE}"
